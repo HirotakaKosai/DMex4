432a35b6cd06da740a67dce930f4d364a7c1a911
HirotakaKosai <s1260155@u-aizu.ac.jp>
2019-07-11 18:30:33 +0900

    Won/lose revision

--- DiceGame.java
+++ DiceGame.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class DiceGame {
 	public static void main(String[] args) {
@@ -7,11 +9,23 @@ public class DiceGame {
 	int a = r.nextInt(6);
 	int b = r.nextInt(6);
 	int total=a+b;
+
+	System.out.println("What is your name ?");
+
+	Scanner scan = new Scanner(System.in);
+	System.out.print(">");
+	String str = scan.next();
+	System.out.println("Hello, " + str + "!");
 	
 	System.out.println("Rolling the dice...");
 	System.out.println("Die 1:"+a);
 	System.out.println("Die 2:"+b);
 	System.out.println("Total Value:"+total);
+	
+	if(total>7) {
+		System.out.println("You won!");
+	}else {
+		System.out.println("You lose");
+		}
 	}
 }
-
